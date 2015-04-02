8f58c5ed4897d118fea0201eb20f1ff20328d47f
riahk <moar.riah@gmail.com>
2015-04-02 10:49:47 -0700

    modified tests

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -5,7 +5,7 @@ describe('solvers', function() {
 
     it('finds a valid solution for n of 1-8', function() {
       _.range(1, 9).map(function(n) {
-        var solutionBoard = new Board(findNRooksSolution(n));
+        var solutionBoard = new Board(findNRooksSolution(n).rows());
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
           return memo + _.reduce(row, function(memo, col) {
             return memo + col;
@@ -38,7 +38,7 @@ describe('solvers', function() {
     it('finds a valid solution for n of 0-7', function() {
       // Skip 2 and 3 because they have no solution.
       [0, 1, 4, 5, 6, 7, 8].map(function(n) {
-        var solutionBoard = new Board(findNQueensSolution(n));
+        var solutionBoard = new Board(findNQueensSolution(n).rows());
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
           return memo + _.reduce(row, function(memo, col) {
             return memo + col;
@@ -52,7 +52,7 @@ describe('solvers', function() {
 
       // Check 2 and 3 for no solution
       [2, 3].map(function (n) {
-        var solutionBoard = new Board(findNQueensSolution(n));
+        var solutionBoard = new Board(findNQueensSolution(n).rows());
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
           return memo + _.reduce(row, function(memo, col) {
             return memo + col;
